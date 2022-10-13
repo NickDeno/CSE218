64d6eddf60f55163cfb3818cea9319e6ffb7de56
NickDeno <nickdenobrega@gmail.com>
2022-10-13 02:17:54 -0400

    CSE218 Content

--- HW05/src/p1_lyricsLinkedList/ParentList.java
+++ HW05/src/p1_lyricsLinkedList/ParentList.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.LinkedList;
 import java.util.ListIterator;
-import java.util.Random;
 import java.util.Scanner;
 
 public class ParentList {
