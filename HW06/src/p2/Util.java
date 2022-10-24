2e9bf63fd67d559600399eb3b8e378af03e1dd08
NickDeno <nickdenobrega@gmail.com>
2022-10-24 15:56:24 -0400

    CSE218 Content

--- HW06/src/p2/Util.java
+++ HW06/src/p2/Util.java
@@ -2,7 +2,7 @@ package p2;
 
 public class Util {
 	public static String stringReverser(String str) {
-		if(str.length() == 1) return str;
+		if(str.length() <= 1) return str;
 		return str.charAt(str.length()-1) + stringReverser(str.substring(0, str.length()-1));	
 	}
 }
