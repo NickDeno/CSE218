036ec4392f2b819461f0cde8615d4bcd7ed0bea9
Nick DeNobrega <nickdenobrega@gmail.com>
2022-10-24 15:42:20 -0400

    CSE218 Content

--- HW06/src/p3/Util.java
+++ HW06/src/p3/Util.java
@@ -1,12 +1,12 @@
 package p3;
 
 public class Util {
-	public static <E extends Number & Comparable<E>> E maxElement(E[] arr, int idx) {
+	public static <E extends Comparable<E>> E maxElement(E[] arr, int idx) {
 		if(idx == arr.length-1) return arr[idx];
 		return max(arr[idx], maxElement(arr, idx+1));	
 	}
 
-	private static <E extends Number & Comparable<E>> E max(E e1, E e2) {
+	private static <E extends Comparable<E>> E max(E e1, E e2) {
 		if(e1.compareTo(e2) > 1) return e1;
 		if(e1.compareTo(e2) < 1) return e2;
 		return e1;
